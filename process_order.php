6789f76134ded3915d1fe719416c1e4a09a1f927
SinghBavandeep <115554428+SinghBavandeep@users.noreply.github.com>
2023-12-12 22:49:19 +0100

    v-1

--- process_order.php
+++ process_order.php
@@ -1,9 +1,31 @@
 <?php
+// process_order.php
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
 include('include/ConnectionBD/db_connection.php');
+
+function query_database($q) {
+    $conn = connect_to_database();
+    $query = $q;
+    $result = $conn->query($query);
+
+    if (!$result) {
+        die("Query execution failed: " . $conn->error);
+    }
+
+    $tableData = array();
+
+    while ($row = $result->fetch_assoc()) {
+        $tableData[] = $row;
+    }
+
+    $result->close();
+    $conn->close();
+
+    return $tableData;
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['order'])) {
     if (!isset($_SESSION['role'])) {
@@ -26,19 +48,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['order'])) {
 
     $conn = connect_to_database();
 
+    // Insertion de la commande
     $insertCommandeQuery = "";
 
     switch ($role) {
         case 'admin':
-            $insertCommandeQuery = "INSERT INTO commande (userEmail, dateCommande) VALUES (?, ?)";
-            break;
-
         case 'client':
-            $insertCommandeQuery = "INSERT INTO commande (userEmail, dateCommande) VALUES (?, ?)";
-            break;
-
         case 'employe':
-            $insertCommandeQuery = "INSERT INTO commande (userEmail, dateCommande) VALUES (?, ?)";
+            $insertCommandeQuery = "INSERT INTO commande (userEmail, dateCommande, produit) VALUES (?, ?, ?)";
             break;
 
         default:
@@ -47,13 +64,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['order'])) {
     }
 
     $stmt = $conn->prepare($insertCommandeQuery);
-    $stmt->bind_param("ss", $userEmail, $currentDate);
+
+    // Obtenez le contenu du panier
+    $queryCartItems = "SELECT nom, quantite, totalprice FROM panier";
+    $cartItems = query_database($queryCartItems);
+
+    // Construisez une chaîne avec le contenu du panier
+    $produit = "";
+    foreach ($cartItems as $item) {
+        $produit .= $item['nom'] . " (Quantité: " . $item['quantite'] . ", Prix: " . $item['totalprice'] . "), ";
+    }
+
+    // Supprimez la virgule et l'espace à la fin de la chaîne
+    $produit = rtrim($produit, ', ');
+
+    $stmt->bind_param("sss", $userEmail, $currentDate, $produit);
 
     if ($stmt->execute()) {
-        // Order successfully added, now clear the cart and redirect
-        unset($_SESSION['cart']);  // Assuming 'cart' is the session variable storing the cart data
+        // Commande ajoutée avec succès
 
-        // Redirect to index.php
+        // Suppression du contenu du panier avec TRUNCATE TABLE
+        $truncateQuery = "TRUNCATE TABLE panier";
+        $conn->query($truncateQuery);
+
+        // Redirection vers index.php
         header("Location: index.php");
         exit();
     } else {
